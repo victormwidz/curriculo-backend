b494edda41640192b62467d61f668f1df92b32cd
victor matheus widz <victormwidz@gmail.com>
2022-06-10 19:07:51 -0300

    troca de src para dist

--- ormconfig.js
+++ ormconfig.js
@@ -10,13 +10,13 @@ module.exports = {
         }
     },
     migrations: [
-        'src/database/migrations/**/*'
+        'dist/database/migrations/**/*'
     ],
     entities: [
-        'src/database/entities/**/*'
+        'dist/database/entities/**/*'
     ],
     cli: {
-        entitiesDir: 'src/database/entities',
-        migrationsDir: 'src/database/migrations'
+        entitiesDir: 'dist/database/entities',
+        migrationsDir: 'dist/database/migrations'
     }
 };
